aa426a8831a80203ea72197fa4b33be3666a64c8
Richard Hull <rm_hull@yahoo.co.uk>
2025-11-23 00:21:39 +0000

    fix: reintroduce previously removed method

--- parser/state.go
+++ parser/state.go
@@ -13,3 +13,7 @@ func NewState(r Reader) *State {
 func (st *State) Location() Location {
 	return st.Input.CurrentLocation()
 }
+
+func (st *State) Remaining() string {
+	return st.Input.Slice(st.Input.CurrentLocation().Index, st.Input.BufferedLength())
+}
